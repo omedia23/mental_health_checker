cf1bbacd9d379c66c76338db7fadab7df6150fa3
omedia23 <syedhasan.cse@gmail.com>
2022-07-15 11:52:40 +0600

    done some modifications

--- lib/widgets/task_widget.dart
+++ lib/widgets/task_widget.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class TaskWidget extends StatelessWidget {
-  TaskWidget({Key? key}) : super(key: key);
+class TaskWidget extends StatefulWidget {
+  const TaskWidget({Key? key}) : super(key: key);
 
+  @override
+  State<TaskWidget> createState() => _TaskWidgetState();
+}
+
+class _TaskWidgetState extends State<TaskWidget> {
   final List<Map> _taskList = [
     {
       "image":
@@ -12,7 +17,7 @@ class TaskWidget extends StatelessWidget {
     },
     {
       "image":
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVMVhx1EQixMmhQyQAKG_3fBb0ozOf8KZy-GA2j1V4WLfhqEcYkJrbEE0Y6j6YCmxgs78&usqp=CAU",
+          "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX37224900.jpg",
       "title": "Listen to your favourite tracks",
     },
     {
@@ -29,13 +34,19 @@ class TaskWidget extends StatelessWidget {
       itemCount: _taskList.length,
       itemBuilder: (context, index) {
         return GestureDetector(
-          onTap: () {},
+          onTap: () {
+            setState(() {
+              _taskList.remove(index);
+            });
+          },
           child: Card(
             child: Column(
               mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Image.network(_taskList[index]["image"]),
+                Image.network(
+                  _taskList[index]["image"],
+                ),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Text(
